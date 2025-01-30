de1b41a80654e9018fc74b8a283b2c09754a9d06
biraj-dahal <dahalbiraj10@gmail.com>
2025-01-30 09:24:12 -0500

    Instaparse clone lab completed

--- lab-insta-parse/lab-insta-parse/Models/Post.swift
+++ lab-insta-parse/lab-insta-parse/Models/Post.swift
@@ -8,11 +8,26 @@
 import Foundation
 
 // TODO: Pt 1 - Import Parse Swift
+import ParseSwift
+
 
 
 // TODO: Pt 1 - Create Post Parse Object model
 // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
 
-struct Post {
+struct Post: ParseObject {
+    var originalData: Data?
     
+    var objectId: String?
+    
+    var createdAt: Date?
+    
+    var updatedAt: Date?
+    
+    var ACL: ParseSwift.ParseACL?
+    
+    // Your own custom properties.
+    var caption: String?
+    var user: User?
+    var imageFile: ParseFile?
 }
